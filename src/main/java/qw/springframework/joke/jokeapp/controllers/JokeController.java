a77a89ebcd25d8a430b8f03b50d04c994d8196b6
Qi Wu <qi.wu@elliemae.com>
2018-10-06 11:52:04 -0700

    Joke controller

---
+++ src/main/java/qw/springframework/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package qw.springframework.joke.jokeapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import qw.springframework.joke.jokeapp.services.JokeService;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
